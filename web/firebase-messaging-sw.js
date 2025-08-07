12de2270dff054179e7c5b143c84f45f264b2705
Lungelo Ngcemu <lungelofarm@gmail.com>
2025-08-07 18:30:12 +0200

    now now

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js");
+importScripts('https://www.gstatic.com/firebasejs/9.9.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.9.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
     apiKey: "AIzaSyC68W0ODoBk8taha9W08fVZcRbHUEbYUm0",
